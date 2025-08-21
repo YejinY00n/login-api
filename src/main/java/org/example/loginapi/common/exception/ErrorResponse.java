45f23ea2e4fd9e19677e249c80430e201c73d7d9
YejinY00n <yoon17710@gmail.com>
2025-08-21 22:52:20 +0900

    Docs: Swagger API 응답 예시 추가

--- src/main/java/org/example/loginapi/common/exception/ErrorResponse.java
+++ src/main/java/org/example/loginapi/common/exception/ErrorResponse.java
@@ -4,18 +4,30 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 public class ErrorResponse {
-
+	@Schema(description = "에러 정보", example = """
+		{
+		  "error": {
+		    "code": "ERROR_CODE",
+		    "message": "에러 메세지입니다."
+		  }
+		}
+		""")
 	private ErrorDetail error;
 
 	@Getter
 	@AllArgsConstructor
 	@NoArgsConstructor
 	public static class ErrorDetail {
+		@Schema(description = "에러 코드", example = "ERROR_CODE")
 		private String code;
+
+		@Schema(description = "에러 메시지", example = "에러 메세지입니다.")
 		private String message;
 	}
 
